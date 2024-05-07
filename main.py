53e3c624e619c6b9207e7373ed0aa7707e429d29
Sean Palastro <spalastro@aigcontrols.com>
2024-05-07 16:03:53 -0400

    context injection testing

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
-from base64 import urlsafe_b64encode
-from operator import methodcaller
-from flask import Flask, jsonify, request, send_file, render_template
+from flask import Flask, jsonify, request, send_file, render_template, redirect
 from pycomm3 import LogixDriver
 import json
 
-PLC_IP = "192.168.1.1"
-PLCTAG = "Reactors[0]"
+PLC_IP = "127.0.0.1"
+PLCTAG = "Reactors[0].Calcs"
 
 
 
@@ -19,8 +17,8 @@ app = Flask(__name__)
 def index():  
     global PLC_IP, PLCTAG
     if request.method == 'GET':
-        str = "IP Address:" + PLC_IP + " Tagname:" + PLCTAG
-        return str, 200
+        context={'ip': PLC_IP, 'tag': PLCTAG}
+        return render_template("index.html", **context)
     
     if request.method == 'POST':
         PLC_IP = request.form.get('ip')
@@ -28,16 +26,35 @@ def index():
         str = "IP Address:" + PLC_IP + " Tagname: " + PLCTAG
         print("IP and tag changed to: " + PLC_IP + PLCTAG)
         return str, 200
+    
+
+@app.route('/config', methods=['GET', 'POST'])
+def config():
+    global PLC_IP, PLCTAG
+    if request.method == 'POST':
+        PLC_IP = request.form.get('ip')
+        PLCTAG = request.form.get('tag')
+        str = "Parameters changed successfully - IP Address:" + PLC_IP + " Tagname: " + PLCTAG
+        print("IP and tag changed to: " + PLC_IP + PLCTAG)
+        return redirect('/')
+    
+    if request.method == 'GET':
+        return render_template('config.html'), 200
+
+
+
+
 
 
 @app.route('/result',methods=['GET'])
 def result():
     if request.method == 'GET':
-        with LogixDriver(PLC_IP) as plc:
+        with LogixDriver(PLC_IP,) as plc:
             print("reading " + PLCTAG + " from " + PLC_IP)
             data = plc.read(PLCTAG)[0:2]
-            pl = json.dumps(data)
-            return '<!DOCTYPE html><html><body><h1>Controller Data</h1><p>' + pl + '</p></body></html>', 200
+            jdata = json.dumps(data)
+            context = {'tag' : jdata }
+            return render_template('result.html',**context), 200
 
 
 @app.route('/favicon.ico', methods=['GET'])
