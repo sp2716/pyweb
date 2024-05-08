daf8c31a74be5016e24f26ffc703f1f62f40751d
Sean Palastro <seanpala@gmail.com>
2024-05-08 13:29:52 -0400

    test

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-
 from flask import Flask, jsonify, request, send_file, render_template, redirect
 from pycomm3 import LogixDriver
 import json
 
 PLC_IP = "127.0.0.1"
 PLCTAG = "Reactors[0].Calcs"
-
+LATEST = "Never ran yet..."
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST', 'DELETE'])
+@app.route('/', methods=['GET'])
 def index():  
     global PLC_IP, PLCTAG
     if request.method == 'GET':
         context={'ip': PLC_IP, 'tag': PLCTAG}
         return render_template("index.html", **context)
-    
-    if request.method == 'POST':
-        PLC_IP = request.form.get('ip')
-        PLCTAG = request.form.get('tag')
-        str = "IP Address:" + PLC_IP + " Tagname: " + PLCTAG
-        print("IP and tag changed to: " + PLC_IP + PLCTAG)
-        return str, 200
     
 
 @app.route('/config', methods=['GET', 'POST'])
@@ -31,8 +23,6 @@ def config():
     if request.method == 'POST':
         PLC_IP = request.form.get('ip')
         PLCTAG = request.form.get('tag')
-        str = "Parameters changed successfully - IP Address:" + PLC_IP + " Tagname: " + PLCTAG
-        print("IP and tag changed to: " + PLC_IP + PLCTAG)
         return redirect('/')
     
     if request.method == 'GET':
@@ -40,15 +30,35 @@ def config():
         return render_template('config.html', **context), 200
 
 
+@app.route('/start', methods=['GET'])
+def start():
+    if request.method == 'GET':
+        global LATEST
+        try:
+            with LogixDriver(PLC_IP,) as plc:
+                    print("reading " + PLCTAG + " from " + PLC_IP)
+                    data = plc.read(PLCTAG)[0:2]
+                    LATEST = json.dumps(data)
+                    context={'ip': PLC_IP, 'tag': PLCTAG, 'payload': LATEST}
+        except:
+                context={'ip': PLC_IP, 'tag': PLCTAG, 'payload': 'comms error'}
+        return render_template("index.html", **context)
+
+
 @app.route('/result',methods=['GET'])
 def result():
     if request.method == 'GET':
-        with LogixDriver(PLC_IP,) as plc:
-            print("reading " + PLCTAG + " from " + PLC_IP)
-            data = plc.read(PLCTAG)[0:2]
-            jdata = json.dumps(data)
-            context = {'payload' : jdata }
-            return render_template('result.html',**context), 200
+        global LATEST, PLCTAG, PLC_IP
+        try:
+            with LogixDriver(PLC_IP,) as plc:
+                    print("reading " + PLCTAG + " from " + PLC_IP)
+                    data = plc.read(PLCTAG)[0:2]
+                    LATEST = json.dumps(data)
+                    context = {'payload' : LATEST }
+        except:
+                context = {'payload': 'comms error'}
+            
+        return render_template('result.html',**context), 200
 
 
 @app.route('/favicon.ico', methods=['GET'])
